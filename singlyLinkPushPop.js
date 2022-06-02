1c5116c84f7834bcb1501a63f3cf695b0a55503b
Milad64sh <m.shalikarian@gmail.com>
2022-06-02 07:36:12 -0700

    singly link

--- singlyLinkPushPop.js
+++ singlyLinkPushPop.js
@@ -40,21 +40,7 @@ class SinglyLinkedList {
 
     return current;
   }
-
-  shift() {
-    if (!this.head) return undefined;
-    var currentHead = this.head;
-    this.head = currentHead.next;
-    this.length--;
-    if (this.length === 0) {
-      this.tail = null;
-    }
-    return currentHead;
-  }
 }
 var list = new SinglyLinkedList();
 list.push('hi');
 list.push('there');
-list.push('!');
-list.shift();
-console.log(list);
